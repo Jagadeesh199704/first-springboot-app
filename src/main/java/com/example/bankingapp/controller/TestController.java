c0335bf0c0813ce714421a3b54955e33aadc5b71
Jagadeesh <jaggu5733@gmail.com>
2022-04-27 18:20:18 +0530

    added path variables and methods

--- src/main/java/com/example/bankingapp/controller/TestController.java
+++ src/main/java/com/example/bankingapp/controller/TestController.java
@@ -1,49 +1,23 @@
 package com.example.bankingapp.controller;
 
+import com.example.bankingapp.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class TestController {
-    // endpoint
-    @GetMapping("test")
-    public String myFirstMethod(){
 
-        return "Learning spring-boot!!";
-    }
-
-     @GetMapping("num/{num1}/{num2}")//path variable
-    public int myFirstMethod(@PathVariable int num1, @PathVariable int num2){
-        try{
-            return num1+ num2;
-        }catch (NumberFormatException d){
-            throw new NullPointerException("Number and string cannot be added");
-        }
-    }
+    @Autowired
+    private TestService testService;
 
     @GetMapping("product-details/{productName}")
-    public String myFirstMethod(@PathVariable String productName){
-        switch (productName){
-            case "Washing Machine":
-                return electronicsDeatils();
-            case  "LED TV":
-                return electronicsDeatil();
-            default:
-                return "No Products Found!!!";
-        }
-    }
+    public String myFirstMethod(@PathVariable String productName) {
 
-    public String electronicsDeatils(){
-        return "Samsung Washing Machine Model No.WW61, Sl.No. 1254321SA45";
+        return testService.fetchProductDetails(productName);
 
     }
-
-    public String electronicsDeatil(){
-        return "LG LED TV Model:LG001, Sl.No: 12254, Price: 25000";
-    }
-
 }
 
 
